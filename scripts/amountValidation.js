6737573e4d1d5222737626f7537c9ab93f3411b3
kiribu <129784782+kiryshabutor@users.noreply.github.com>
2023-04-08 16:20:58 +0300

    add styles to disablead button

--- scripts/amountValidation.js
+++ scripts/amountValidation.js
@@ -11,12 +11,13 @@ function onAmountInput() {
     if( amountInput.value.length == 0 ){
         amountInputForm.classList.remove('input__value--incorrect')
         amountInputForm.classList.remove('input__value--correct')
+        amountButton.style.background = '#767474';
         amountButton.style.pointerEvents = 'none';
     }
     else if ( !isNaN( Number(amountInput.value) ) ) {
         amountInputForm.classList.remove('input__value--incorrect')
         amountInputForm.classList.add('input__value--correct')
-        amountButton.style.borderColor = '#4B9200';
+        amountButton.style.background = 'linear-gradient(113.96deg, #F9804B 1.49%, #FE9013 101.44%)';
         amountButton.style.pointerEvents = 'all';
     } else {
         amountInputForm.classList.remove('input__value--correct')
