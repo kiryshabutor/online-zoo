45f49d4bb3e6e9f72e65be8ffe175e81c9a89d49
kiribu <129784782+kiryshabutor@users.noreply.github.com>
2023-04-14 17:54:23 +0300

    prettier format

--- scripts/lazyLoad.js
+++ scripts/lazyLoad.js
@@ -1,26 +1,28 @@
-"use strict";
- 
-const lazyImages = Array.from(document.querySelectorAll("img[data-src]"));
+'use strict';
+
+const lazyImages = Array.from(document.querySelectorAll('img[data-src]'));
 const windowHeight = document.documentElement.clientHeight;
- 
+
 let lazyImagesPositions = [];
 if (lazyImages.length > 0) {
     lazyImages.forEach((img) => {
         if (img.dataset.src) {
-            lazyImagesPositions.push(img.getBoundingClientRect().top + pageYOffset);
+            lazyImagesPositions.push(
+                img.getBoundingClientRect().top + pageYOffset
+            );
         }
     });
     lazyScrollCheck();
 }
- 
-window.addEventListener("scroll", lazyScroll);
- 
+
+window.addEventListener('scroll', lazyScroll);
+
 function lazyScroll() {
-    if (document.querySelectorAll("img[data-src]").length > 0) {
+    if (document.querySelectorAll('img[data-src]').length > 0) {
         lazyScrollCheck();
     }
 }
- 
+
 function lazyScrollCheck() {
     let imagesIndexes = [];
     lazyImagesPositions.forEach((imagePosition, index) => {
@@ -32,8 +34,8 @@ function lazyScrollCheck() {
         imagesIndexes.forEach((imgIndex) => {
             if (lazyImages[imgIndex].dataset.src) {
                 lazyImages[imgIndex].src = lazyImages[imgIndex].dataset.src;
-                lazyImages[imgIndex].removeAttribute("data-src");
-            } 
+                lazyImages[imgIndex].removeAttribute('data-src');
+            }
             delete lazyImagesPositions[imgIndex];
         });
     }
--- scripts/removeButton.js
+++ scripts/removeButton.js
@@ -1,28 +1,28 @@
-'use strict'
+'use strict';
 
-const returnButton = document.querySelector('.button-back')
-const header = document.querySelector('.header')
+const returnButton = document.querySelector('.button-back');
+const header = document.querySelector('.header');
 
-let scrollPos = 0
+let scrollPos = 0;
 
-setTimeout(addOpacityStyle, 100)
+setTimeout(addOpacityStyle, 100);
 
-function addOpacityStyle(){
-  returnButton.style.transition = 'opacity .5s'
+function addOpacityStyle() {
+    returnButton.style.transition = 'opacity .5s';
 }
 
-window.addEventListener('scroll', function() {
-  if( window.pageYOffset >= ( header.clientHeight - this.document.querySelector('.header__menu').clientHeight ) )
-    returnButton.classList.add('active')
-  else
-    returnButton.classList.remove('active')
+window.addEventListener('scroll', function () {
+    if (
+        window.pageYOffset >=
+        header.clientHeight -
+            this.document.querySelector('.header__menu').clientHeight
+    )
+        returnButton.classList.add('active');
+    else returnButton.classList.remove('active');
 });
-console.log(document.querySelector('.header').clientHeight)
-returnButton.addEventListener('click', function(e){
-  e.preventDefault();
-  var scroll = new SmoothScroll()
-  scroll.animateScroll(header);
+console.log(document.querySelector('.header').clientHeight);
+returnButton.addEventListener('click', function (e) {
+    e.preventDefault();
+    var scroll = new SmoothScroll();
+    scroll.animateScroll(header);
 });
-
-
-
